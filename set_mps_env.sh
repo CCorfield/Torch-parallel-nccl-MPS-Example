657f311d267947f9be2235f7716f4c65ccce5f65
CCorfield <cnc@Lhotse.com>
2016-09-22 07:59:12 -0600

    Update set_mps_env.sh

--- set_mps_env.sh
+++ set_mps_env.sh
@@ -4,7 +4,7 @@
 # Initialize the user environment after starting MPS
 # We look for the pipe directory in /tmp/nvidia-mps* and deduce which devices are in play.
 #
-set -x
+#set -x
 
 # Message
 echo "Setting MPS environment variables..."
@@ -13,20 +13,24 @@ echo "Setting MPS environment variables..."
 suffix=''
 devices=''
 pipedir=`ls /tmp | grep 'nvidia-mps'`
-
-if [ "$pipedir" != '' ]; then
-  suffix=`echo $pipedir | sed 's,nvidia-mps_\?,,'`
-  devices=${suffix//_/,} # Convert underscores to commmas
-fi
-
-# Suffix is either a comma separated list of devices, or blank
-if [ "$devices" == '' ]; then
-  export CUDA_MPS_PIPE_DIRECTORY=/tmp/nvidia-mps
-  export CUDA_MPS_LOG_DIRECTORY=/var/log/nvidia-mps
+count=`echo $pipedir | wc -w`
+if [ "$count" -gt '1' ]; then
+  echo "There is more than one MPS environment defined."
+  echo "Source set_mps_env_for_gpus.sh <gpu1>[,<gpu2>,...] instead"
 else
-  export CUDA_VISIBLE_DEVICES=$devices
-  export CUDA_MPS_PIPE_DIRECTORY=/tmp/nvidia-mps_$suffix
-  export CUDA_MPS_LOG_DIRECTORY=/var/log/nvidia-mps_$suffix
+  if [ "$pipedir" != '' ]; then
+    suffix=`echo $pipedir | sed 's,nvidia-mps_\?,,'`
+    devices=${suffix//_/,} # Substitute commas
+  fi
+
+  # Suffix is either a comma separated list of devices, or blank
+  if [ "$devices" == '' ]; then
+    export CUDA_MPS_PIPE_DIRECTORY=/tmp/nvidia-mps
+    export CUDA_MPS_LOG_DIRECTORY=/var/log/nvidia-mps
+  else
+    export CUDA_MPS_PIPE_DIRECTORY=/tmp/nvidia-mps_$suffix
+    export CUDA_MPS_LOG_DIRECTORY=/var/log/nvidia-mps_$suffix
+  fi
 fi
 
-set +x
+#set +x
